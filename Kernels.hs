5bb641fde34d2ca9fbd5b2e29136d9f26572e51b
Praveen Narayanan <pravnar@snorkel.cs.indiana.edu>
2014-03-28 00:02:31 -0400

    Work on kernels
    Some back-and-forth between type definitions.
    Trying to make it work with random-fu and the multiparameter
    typeclass. random-fu doens't have densities so perhaps we need
    to go back to statistics..

--- Kernels.hs
+++ Kernels.hs
@@ -3,15 +3,25 @@
 module Kernels where
 
 import Data.Random.Distribution as D
+import Data.Random.Distribution.Uniform
+import Data.Random.RVar
 
 class Kernel k where
-    sample :: k a -> a
+    sample :: k a -> RVar a
+
+-- data MetropolisHastings a where
+--     MH :: (Distribution p a, Distribution q a) => p a -> (a -> q a) -> MetropolisHastings a
 
 data MetropolisHastings a where
-    MH :: (Distribution p a, Distribution q a) => p a -> (a -> q a) -> MetropolisHastings a
+    MH :: (Distribution p a) => p a -> MetropolisHastings a 
+
+-- instance Kernel MetropolisHastings where
+--     sample _ = undefined
 
 instance Kernel MetropolisHastings where
-    sample _ = undefined
+    sample (MH t) = do
+      u <- stdUniform :: RVar Double
+      if u < 0.5 then rvar t else rvar t
 
 data CoolingSchedule
 
@@ -21,11 +31,12 @@ data SimulatedAnnealing a where
 instance Kernel SimulatedAnnealing where
     sample _ = undefined
 
-data Mixture k l a where
-    Mixture :: (Kernel k, Kernel l) => k a -> l a -> Mixture k l a
+data Mixture k l a = Mix (k a) (l a) Double
 
-instance Kernel (Mixture k l) where
-    sample _ = undefined
+instance (Kernel k, Kernel l) => Kernel (Mixture k l) where
+    sample (Mix ka la nu) = do
+      u <- stdUniform
+      if u < nu then sample ka else sample la
 
 -- mixture :: (Kernel k, Kernel l) => k a -> l a -> Mixture k l a
 -- mixture = undefined
