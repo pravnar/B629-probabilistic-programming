5e3377ab2894623c9479fdaf94007859948d356f
Praveen Narayanan <revenap@gmail.com>
2014-04-15 02:52:43 -0400

    Changing the type of 'walk'
    - Made 'walk' take as input an action and additional state info
    - This is to generalize the code to make it work with both the
    	visualizer, and any other monadic action that we might want to
    	take in the middle of the sampling process
    - Also added a slightly general function to print the n latest samples
    	- This calls viz_json and as such needs a way to get from a list of
    		any type to a list of Doubles for now

--- Kernels.hs
+++ Kernels.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE EmptyDataDecls, GADTs, MultiParamTypeClasses, KindSignatures, FlexibleInstances #-}
 
 module Kernels ( Kernel (..)
+               , Chain
+               , St
+               , Action
+               , print_latest_samples
                , MetropolisHastings
                , metropolis_hastings
+               , print_latest_mh_samples
                , SimulatedAnnealing
                , simulated_annealing
+               , print_latest_sa_samples
                , Mixture
                , mixture
                , Temp
@@ -15,25 +21,50 @@ module Kernels ( Kernel (..)
 import Distributions
 import Control.Monad.Primitive
 import qualified System.Random.MWC as MWC
+import Control.Monad
+
+type Rand m = MWC.Gen (PrimState m)
+type JumpSize = Int
+type NumSamples = Int
+type Chain = (NumSamples, JumpSize)
+type St x = ([x], Either Int Int)
+type Action m x = St x -> m (St x)
 
 class Kernel (k :: *) (x :: *) where
-    step :: PrimMonad m => k -> x -> MWC.Gen (PrimState m) -> m x
-    
-    nsteps :: PrimMonad m => k -> x -> Int -> MWC.Gen (PrimState m) -> m x
+    step :: PrimMonad m => k -> x -> Rand m -> m x
+            
+    nsteps :: PrimMonad m => k -> x -> JumpSize -> Rand m -> m x
     nsteps _ x0 0 _ = return x0
     nsteps k x0 n g = do
       x <- step k x0 g
       nsteps k x (n-1) g
     
-    walk :: PrimMonad m => k -> [x] -> Int -> Int -> MWC.Gen (PrimState m) -> m [x]
-    walk _ xl 0 _ _ = return xl
-    walk k (x:xs) n m g = do
-      xm <- nsteps k x m g
-      walk k (xm:x:xs) (n-1) m g
+    walk :: PrimMonad m => k -> St x -> Chain -> Action m x -> Rand m -> m (St x)
+    walk k (xl@(x:_), Left i) (n,m) f rng
+        | n == 1 = f (xl, Right i)
+        | n > 1 = do xm <- nsteps k x m rng
+                     st <- f ((xm:xl), Left (i+1))
+                     walk k st ((n-1), m) f rng
+        | otherwise = error "Kernel:walk called with #samples < 1"
+    walk _ st@(_, Right _) _ _ _ = return st
 
--- data MetropolisHastings a where
---     MH :: (Distribution t a, Sampleable p a) => 
---           t a -> (a -> p a) -> MetropolisHastings a
+-- Visualization --
+
+viz_json :: [Double] -> Int -> Int -> String
+viz_json samplelist current total = "{\"current_sample\": " ++ show current
+                                    ++ ", \"total_samples\": " ++ show total
+                                    ++ ", \"rvars\": {\"x\": " ++ show samplelist
+                                    ++ "}}"
+
+print_latest_samples :: ([a] -> [Double]) -> Int -> Int -> Action IO a
+print_latest_samples f n total st@(xl, Left i) = do
+  when (i `mod` n == 0) $ putStrLn (viz_json (take n $ f xl) i total)
+  return st
+print_latest_samples f n total st@(xl, Right i) = do
+  putStrLn (viz_json (take (i `mod` n) $ f xl) total total)
+  return st
+
+-- Metropolis Hastings --
 
 data MetropolisHastings t p a = MH (t a) (a -> p a) 
 
@@ -49,12 +80,13 @@ instance (Distribution t a, Sampleable p a) => Kernel (MetropolisHastings t p a)
           denom = (density t xi) * (density (c_p xi) xstar)
       if u < accept then return xstar else return xi
 
+print_latest_mh_samples :: Int -> Int -> Action IO Double
+print_latest_mh_samples = print_latest_samples id
+
+-- Simulated Annealing --
+
 type Temp = Double
 type CoolingSchedule = Temp -> Temp
-
--- data SimulatedAnnealing a where
---     SA :: (Distribution t a, Sampleable p a) => 
---           t a -> (a -> p a) -> SimulatedAnnealing a
 
 data SimulatedAnnealing t p a = SA (t a) (a -> p a) 
 
@@ -70,6 +102,14 @@ instance (Distribution t a, Sampleable p a) => Kernel (SimulatedAnnealing t p a)
           denom = (*) (density (c_p xi) xstar) $ (**) (1 / temp) (density t xi)
           new_temp = cool temp
       if u < accept then return (xstar,new_temp,cool) else return (xi,new_temp,cool)
+
+first :: (a, b, c) -> a
+first (a,_,_) = a
+
+print_latest_sa_samples :: Int -> Int -> Action IO (Double, Temp, CoolingSchedule)
+print_latest_sa_samples = print_latest_samples (map first)
+
+-- Kernel Mixtures --
 
 type MixRatio = Double
 
--- Tests.hs
+++ Tests.hs
@@ -5,7 +5,6 @@ module Main where
 import Distributions
 import Kernels
 import qualified System.Random.MWC as MWC
-import Control.Monad
 
 data ExampleTarget a = ET
 
@@ -21,42 +20,33 @@ gaussian_proposal x = normal x 100
 example_mh_kernel :: MetropolisHastings ExampleTarget Normal Double
 example_mh_kernel = metropolis_hastings ET gaussian_proposal
 
-mh_test_run :: IO [Double]
+chain1 :: Chain
+chain1 = (10000, 100)
+
+mh_test_run :: IO (St Double)
 mh_test_run = do
   g <- MWC.createSystemRandom
-  l <- walk example_mh_kernel [0] 100 100 g
-  putStrLn $ viz_json l
-  return l
+  let act = print_latest_mh_samples 50 (fst chain1)
+  walk example_mh_kernel ([0], Left 1) chain1 act g
 
 example_sa_kernel :: SimulatedAnnealing ExampleTarget Normal Double
 example_sa_kernel = simulated_annealing ET gaussian_proposal
 
-sa_test_run :: IO [Double]
+sa_test_run :: IO (St (Double, Temp, CoolingSchedule))
 sa_test_run = do
   g <- MWC.createSystemRandom
-  let cool_sch t = t / 1.125 :: Temp 
-      -- cool_sch = (*) (1 - 1e-5) :: Temp -> Temp
-      first (a,_,_) = a
+  let -- cool_sch t = t / 1.125 :: Temp 
+      cool_sch = (*) (1 - 1e-3) :: Temp -> Temp
       init_temp = 1 :: Temp
-  ls <- walk example_sa_kernel [(0, init_temp, cool_sch)] 1000 10 g
-  let l = my_filter $ map first ls
-  putStrLn $ viz_json l
-  return l
+      initial = [(0, init_temp, cool_sch)]
+      act = print_latest_sa_samples 50 (fst chain1)
+  walk example_sa_kernel (initial, Left 1) chain1 act g
 
-my_filter :: [Double] -> [Double]
-my_filter = filter (((>) 20) . abs)
-
-viz_json :: [Double] -> String
-viz_json samplelist = 
-    let l = length samplelist
-    in "{\"current_sample\": " ++ show l
-           ++ ", \"total_samples\": " ++ show l
-           ++ ", \"rvars\": {\"x\": " ++ show samplelist
-           ++ "}}"
+-- my_filter :: [Double] -> [Double]
+-- my_filter = filter (((>) 20) . abs)
 
 main :: IO ()
-main = do 
-  -- replicateM_ 100 mh_test_run
-  replicateM_ 100 sa_test_run
-  -- salist <- sa_test_run
-  -- putStrLn $ viz_json salist
+main = do
+  -- mh_test_run
+  sa_test_run
+  return ()
