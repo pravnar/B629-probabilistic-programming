955382c0417ca29148a14402f1ce7e15b84aa207
Praveen Narayanan <pravnar@snorkel.cs.indiana.edu>
2014-04-25 20:39:32 -0400

    Simplifying Kernels
    - Moving away from typeclasses and data structure representations
    - Moving towards a procedural representation of MH and SA
    - One common datatype 'Kernel'
    - Changed kernelMix to reflect this as well

--- Kernels.hs
+++ Kernels.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE GADTs, MultiParamTypeClasses, KindSignatures, FlexibleInstances #-}
 
 module Kernels ( Kernel (..)
-               , MetropolisHastings
+               , walk
+               -- , MetropolisHastings
                , metropolisHastings
                , vizMH
                , Temp
                , CoolingSchedule
                , MixRatio
-               , SimulatedAnnealing
+               -- , SimulatedAnnealing
                , StSA
                , simulatedAnnealing
                , vizSA
-               , KernelMixture
+               -- , KernelMixture
                , kernelMix
                ) where
 
@@ -23,31 +24,50 @@ import qualified System.Random.MWC as MWC
 -- Kernels --
 
 type Rand m = MWC.Gen (PrimState m)
+type Step x m = x -> Rand m -> m x
 
-class Kernel (k :: *) (x :: *) where
-    step :: PrimMonad m => k -> x -> Rand m -> m x
+data Kernel x m where
+    K :: Step x m -> Kernel x m
+
+walk :: PrimMonad m => Kernel x m -> x -> Int -> Rand m -> Action x m a b -> m b
+walk _ _ 0 _ (Action _ f a) = f a
+walk k@(K step) x n r action = do 
+  x' <- step x r
+  execute action x' >>= walk k x' (n-1) r
+
+-- class Kernel (k :: *) (x :: *) where
+--     step :: PrimMonad m => k -> x -> Rand m -> m x
     
-    walk :: PrimMonad m => k -> x -> Int -> Rand m -> Action x m a b -> m b
-    walk _ _ 0 _ (Action _ f a) = f a
-    walk k x n r action = do 
-      x' <- step k x r
-      execute action x' >>= walk k x' (n-1) r
+--     walk :: PrimMonad m => k -> x -> Int -> Rand m -> Action x m a b -> m b
+--     walk _ _ 0 _ (Action _ f a) = f a
+--     walk k x n r action = do 
+--       x' <- step k x r
+--       execute action x' >>= walk k x' (n-1) r
 
 -- Metropolis Hastings --
 
-data MetropolisHastings t p a = MH (t a) (a -> p a) 
+-- data MetropolisHastings t p a = MH (t a) (a -> p a) 
 
-metropolisHastings :: t a -> (a -> p a) -> MetropolisHastings t p a
-metropolisHastings = MH
+metropolisHastings :: (Distribution t a, Sampleable p a, PrimMonad m) => 
+                      t a -> (a -> p a) -> Kernel a m
+metropolisHastings t c_p = 
+    let mhStep xi g = do
+          u <- sampleFrom (uniform 0 1) g
+          xstar <- sampleFrom (c_p xi) g
+          let accept = min 1 (numer / denom)
+              numer = density t xstar * density (c_p xstar) xi
+              denom = density t xi * density (c_p xi) xstar
+          return $ if u < accept then xstar else xi
+    in K mhStep
 
-instance (Distribution t a, Sampleable p a) => Kernel (MetropolisHastings t p a) a where
-    step (MH t c_p) xi g = do
-      u <- sampleFrom (uniform 0 1) g
-      xstar <- sampleFrom (c_p xi) g
-      let accept = min 1 (numer / denom)
-          numer = density t xstar * density (c_p xstar) xi
-          denom = density t xi * density (c_p xi) xstar
-      return $ if u < accept then xstar else xi
+-- instance (Distribution t a, Sampleable p a) => Kernel (MetropolisHastings t p a) a where
+--     step (MH t c_p) xi g = do
+--       u <- sampleFrom (uniform 0 1) g
+--       xstar <- sampleFrom (c_p xi) g
+--       let accept = min 1 (numer / denom)
+--           numer = density t xstar * density (c_p xstar) xi
+--           denom = density t xi * density (c_p xi) xstar
+--       return $ if u < accept then xstar else xi
 
 vizMH :: PrintF Double Double
 vizMH = id
@@ -56,23 +76,32 @@ vizMH = id
 
 type Temp = Double
 type CoolingSchedule = Temp -> Temp
-
-data SimulatedAnnealing t p a = SA (t a) (a -> p a) 
-
-simulatedAnnealing :: t a -> (a -> p a) -> SimulatedAnnealing t p a
-simulatedAnnealing = SA
-
 type StSA a = (a, Temp, CoolingSchedule)
 
-instance (Distribution t a, Sampleable p a) => Kernel (SimulatedAnnealing t p a) (StSA a) where
-    step (SA t c_p) (xi,temp,cool) g = do
-      u <- sampleFrom (uniform 0 1) g
-      xstar <- sampleFrom (c_p xi) g
-      let accept = min 1 (numer / denom)
-          numer = (*) (density (c_p xstar) xi) $ (**) (1 / temp) (density t xstar)
-          denom = (*) (density (c_p xi) xstar) $ (**) (1 / temp) (density t xi)
-          new_temp = cool temp
-      return $ if u < accept then (xstar,new_temp,cool) else (xi,new_temp,cool)
+-- data SimulatedAnnealing t p a = SA (t a) (a -> p a) 
+
+simulatedAnnealing :: (Distribution t a, Sampleable p a, PrimMonad m) => 
+                      t a -> (a -> p a) -> Kernel (StSA a) m
+simulatedAnnealing t c_p = 
+    let saStep (xi,temp,cool) g = do
+          u <- sampleFrom (uniform 0 1) g
+          xstar <- sampleFrom (c_p xi) g
+          let accept = min 1 (numer / denom)
+              numer = (*) (density (c_p xstar) xi) $ (**) (1 / temp) (density t xstar)
+              denom = (*) (density (c_p xi) xstar) $ (**) (1 / temp) (density t xi)
+              new_temp = cool temp
+          return $ if u < accept then (xstar,new_temp,cool) else (xi,new_temp,cool)
+    in K saStep
+
+-- instance (Distribution t a, Sampleable p a) => Kernel (SimulatedAnnealing t p a) (StSA a) where
+--     step (SA t c_p) (xi,temp,cool) g = do
+--       u <- sampleFrom (uniform 0 1) g
+--       xstar <- sampleFrom (c_p xi) g
+--       let accept = min 1 (numer / denom)
+--           numer = (*) (density (c_p xstar) xi) $ (**) (1 / temp) (density t xstar)
+--           denom = (*) (density (c_p xi) xstar) $ (**) (1 / temp) (density t xi)
+--           new_temp = cool temp
+--       return $ if u < accept then (xstar,new_temp,cool) else (xi,new_temp,cool)
 
 tripleFirst :: (a, b, c) -> a
 tripleFirst (a,_,_) = a
@@ -87,12 +116,16 @@ vizSA = myFilter . map tripleFirst
 
 type MixRatio = Double
 
-data KernelMixture k l x = KernelMix MixRatio k l
+-- data KernelMixture k l x = KernelMix MixRatio k l
 
-kernelMix :: (Kernel k x, Kernel l x) => MixRatio -> k -> l -> KernelMixture k l x
-kernelMix = KernelMix
+kernelMix :: PrimMonad m => MixRatio -> Kernel x m -> Kernel x m -> Kernel x m
+kernelMix nu (K kstep) (K lstep) = 
+    let mixStep x g = do
+          u <- sampleFrom (uniform 0 1) g
+          if u < nu then kstep x g else lstep x g
+    in K mixStep
 
-instance (Kernel k x, Kernel l x) => Kernel (KernelMixture k l x) x where
-    step (KernelMix nu k l) x g = do
-      u <- sampleFrom (uniform 0 1) g
-      if u < nu then step k x g else step l x g
+-- instance (Kernel k x, Kernel l x) => Kernel (KernelMixture k l x) x where
+--     step (KernelMix nu k l) x g = do
+--       u <- sampleFrom (uniform 0 1) g
+--       if u < nu then step k x g else step l x g
--- Tests.hs
+++ Tests.hs
@@ -19,7 +19,7 @@ instance Distribution ExampleTarget Double where
 gaussianProposal :: Double -> Normal Double
 gaussianProposal x = normal x 100
 
-exampleMH :: MetropolisHastings ExampleTarget Normal Double
+exampleMH :: Kernel Double IO
 exampleMH = metropolisHastings ET gaussianProposal
 
 mhTest :: IO ()
@@ -29,71 +29,71 @@ mhTest = do
       s = skip 100 a
   walk exampleMH 0 (10^6) g s
   
-exampleSA :: SimulatedAnnealing ExampleTarget Normal Double
-exampleSA = simulatedAnnealing ET gaussianProposal
+-- exampleSA :: SimulatedAnnealing ExampleTarget Normal Double
+-- exampleSA = simulatedAnnealing ET gaussianProposal
 
-saTest :: IO ()
-saTest = do
-  g <- MWC.createSystemRandom
-  let -- cool_sch t = t / 1.125 :: Temp 
-      coolSch = (*) (1 - 1e-3) :: Temp -> Temp
-      x0 = (0, 1, coolSch)
-      a = batchPrint vizSA 100
-      s = skip 100 a
-  walk exampleSA x0 (10^6) g s
+-- saTest :: IO ()
+-- saTest = do
+--   g <- MWC.createSystemRandom
+--   let -- cool_sch t = t / 1.125 :: Temp 
+--       coolSch = (*) (1 - 1e-3) :: Temp -> Temp
+--       x0 = (0, 1, coolSch)
+--       a = batchPrint vizSA 100
+--       s = skip 100 a
+--   walk exampleSA x0 (10^6) g s
 
-gauss1 :: Normal2 (Double,Double)
-gauss1 = normal2 (0,0) (scalarCovMatrix 1)
+-- gauss1 :: Normal2 (Double,Double)
+-- gauss1 = normal2 (0,0) (scalarCovMatrix 1)
 
-gauss2 :: Normal2 (Double,Double)
-gauss2 = normal2 (5,5) (scalarCovMatrix 2)
+-- gauss2 :: Normal2 (Double,Double)
+-- gauss2 = normal2 (5,5) (scalarCovMatrix 2)
 
-gaussianMix :: TargetMixture Normal2 Normal2 (Double,Double)
-gaussianMix = targetMix 0.5 gauss1 gauss2
+-- gaussianMix :: TargetMixture Normal2 Normal2 (Double,Double)
+-- gaussianMix = targetMix 0.5 gauss1 gauss2
 
--- 
+-- -- 
 
-proposal1 :: (Double,Double) -> Normal2 (Double,Double)
-proposal1 (x,_) = normal2 (x,2) (scalarCovMatrix 1)
+-- proposal1 :: (Double,Double) -> Normal2 (Double,Double)
+-- proposal1 (x,_) = normal2 (x,2) (scalarCovMatrix 1)
 
-proposal2 :: (Double,Double) -> Normal2 (Double,Double)
-proposal2 (_,y) = normal2 (3,y) (scalarCovMatrix 1)
+-- proposal2 :: (Double,Double) -> Normal2 (Double,Double)
+-- proposal2 (_,y) = normal2 (3,y) (scalarCovMatrix 1)
 
-type MT = MetropolisHastings (TargetMixture Normal2 Normal2) Normal2 (Double,Double)
+-- type MT = MetropolisHastings (TargetMixture Normal2 Normal2) Normal2 (Double,Double)
 
-mh1 :: MT
-mh1 = metropolisHastings gaussianMix proposal1
+-- mh1 :: MT
+-- mh1 = metropolisHastings gaussianMix proposal1
 
-mh2 :: MT
-mh2 = metropolisHastings gaussianMix proposal2
+-- mh2 :: MT
+-- mh2 = metropolisHastings gaussianMix proposal2
 
-mixtureMH :: Kernel MT (Double,Double) => KernelMixture MT MT (Double,Double)
-mixtureMH = kernelMix 0.7 mh1 mh2
+-- mixtureMH :: Kernel MT (Double,Double) => KernelMixture MT MT (Double,Double)
+-- mixtureMH = kernelMix 0.7 mh1 mh2
 
-p1 x = normal x 100
+-- p1 x = normal x 100
 
-p2 x = normal x 50
+-- p2 x = normal x 50
 
-m1 = metropolisHastings ET p1
+-- m1 = metropolisHastings ET p1
 
-m2 = metropolisHastings ET p2
+-- m2 = metropolisHastings ET p2
 
-mixMH = kernelMix 0.7 m1 m2
+-- mixMH = kernelMix 0.7 m1 m2
 
-vizD1 :: [(Double,Double)] -> [Double]
-vizD1 = fmap fst
+-- vizD1 :: [(Double,Double)] -> [Double]
+-- vizD1 = fmap fst
 
-vizD2 :: [(Double,Double)] -> [Double]
-vizD2 = fmap snd
+-- vizD2 :: [(Double,Double)] -> [Double]
+-- vizD2 = fmap snd
 
-viz2D :: [(Double,Double)] -> [(Double,Double)]
-viz2D = id
+-- viz2D :: [(Double,Double)] -> [(Double,Double)]
+-- viz2D = id
 
-mhTest2 = do
-  g <- MWC.createSystemRandom
-  let a = batchPrint vizD2 100
-      s = skip 100 a
-  walk mixtureMH (0,0) (10^6) g s
+-- mhTest2 = do
+--   g <- MWC.createSystemRandom
+--   let a = batchPrint vizD2 100
+--       s = skip 100 a
+--   walk mixtureMH (0,0) (10^6) g s
 
 main :: IO ()
-main = mhTest2
+main = mhTest
